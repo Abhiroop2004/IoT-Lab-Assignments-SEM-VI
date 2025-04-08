9c69945b0666c0d6b92f47dc8d05385f2586aa88
Abhiroop2004 <abhiroopsarkar2004@gmail.com>
2025-04-08 12:05:55 +0530

    modified:   Exp-4/4_photo_resistor.ino

--- Exp-4/4_photo_resistor.ino
+++ Exp-4/4_photo_resistor.ino
@@ -9,7 +9,8 @@ void setup() {
 
 void loop() {
   int lightLevel = analogRead(ldrPin); // Read LDR value (0-1023)
-  int ledbrightness = map(lightLevel, 0, 1023, 0, 255); // Led 0-255
-  analogWrite(ledPin, ledbrightness);
-  delay(10); // Small delay
+  if (lightLevel < 500) 
+  {    digitalWrite(ledPin, HIGH);  } 
+  else 
+  {    digitalWrite(ledPin, LOW);  }
 }
