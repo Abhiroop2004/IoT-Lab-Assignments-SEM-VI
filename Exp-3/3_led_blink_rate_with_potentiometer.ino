843598cd8fbfedfd09a36520b74e3babeff56d7b
Abhiroop2004 <abhiroopsarkar2004@gmail.com>
2025-04-08 12:24:59 +0530

    modified:   Exp-4/4_photo_resistor.ino

--- Exp-3/3_led_blink_rate_with_potentiometer.ino
+++ Exp-3/3_led_blink_rate_with_potentiometer.ino
@@ -3,12 +3,17 @@
 void setup()
 {
   pinMode(9, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  int potValue = analogRead(A2);                   
+  int potValue = analogRead(A2);  
   int ledBrightness = map(potValue, 0, 1023, 0, 255);    // Map to 0-255
-  analogWrite(9, ledBrightness);                 
+  analogWrite(9, ledBrightness);  
+  Serial.print("Pot: ");
+  Serial.print(potValue);
+  Serial.print(" | Brightness: ");
+  Serial.println(ledBrightness); 
   delay(10); // Small delay
 }
